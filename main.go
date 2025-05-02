af7898cdbfa9fe6fb1f12fd85d31964f2dee3e49
deexithparand <deexith2002@gmail.com>
2025-05-03 00:54:02 +0530

    fix : updated origin for render deployment

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "http://localhost:3000,https://velai.onrender.com/",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET,POST,OPTIONS",
 		AllowCredentials: true,
